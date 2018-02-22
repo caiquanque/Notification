01b1c8decf055467fb498cda95646292ad11e137
Dinh Tan Thong <thong.dt@php-thongdt.sutrixmedia1.com>
2018-02-22 13:59:17 +0700

    update 1

--- app/src/main/java/com/example/thongdt/notification/MainActivity.java
+++ app/src/main/java/com/example/thongdt/notification/MainActivity.java
@@ -6,21 +6,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-//    private NetworkChangeReceiver receiver;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        receiver = new NetworkChangeReceiver();
-//        final IntentFilter filter = new IntentFilter("android.net.conn.CONNECTIVITY_CHANE");
-//        registerReceiver(receiver, filter);
     }
-
-//    @Override
-//    protected void onStop() {
-//        super.onStop();
-//        unregisterReceiver(receiver);
-//    }
 }
